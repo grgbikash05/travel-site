bd5cb9f6b189f8523db72d1cc091d715ef7a4c12
Bikash Gurung <gurungbikash145@gmail.com>
2019-05-18 20:24:11 +0545

    Upto Let's build

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,33 @@
 var gulp = require('gulp'),
     browserSync = require('browser-sync').create(),
-    reload = browserSync.reload,
     sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     cssnano = require('gulp-cssnano'),
     sourcemaps = require('gulp-sourcemaps'),
-    concat = require('gulp-concat');
+    concat = require('gulp-concat'),
+    reload = browserSync.reload;
 
-function css() {
-    return gulp.src('./app/assets/sass/**/*.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer())
-    .pipe(cssnano())
-    .pipe(concat('style.min.css'))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('./app/assets/css'))
-    .pipe(browserSync.stream());
-}
+    function css() {
+        return gulp.src('./app/assets/sass/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(cssnano())
+        .pipe(concat('style.min.css'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./app/assets/css'))
+        .pipe(browserSync.stream());
+    }
 
-function watch() {
-    browserSync.init({
-        server: {
-            baseDir: './app'
-        }
-    });
-    gulp.watch('./app/assets/sass/**/*.scss', css);
-    gulp.watch('./app/**/*.html').on('change', reload);
-}
+    function watch() {
+        browserSync.init({
+            server: {
+                baseDir: './app'
+            }
+        });
+        gulp.watch('./app/assets/sass/**/*.scss', css);
+        gulp.watch('./app/**/*.html').on('change', reload);
+    }
 
-exports.css = css;
-exports.watch = watch;
+    exports.css = css;
+    exports.watch = watch;
